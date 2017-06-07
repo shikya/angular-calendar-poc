40cd3ac3a553c204ff106698285e29165dcdf553
Shrikant Sonone <shrikant.sonone@coda.global>
2017-06-07 20:13:51 +0530

    Added custom styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,30 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { CalendarEvent } from 'angular-calendar';
+import {
+  startOfDay,
+  endOfDay,
+  subDays,
+  addDays,
+  endOfMonth,
+  isSameDay,
+  isSameMonth,
+  addHours
+} from 'date-fns';
+
+const colors: any = {
+  red: {
+    primary: '#ad2121',
+    secondary: '#FAE3E3'
+  },
+  blue: {
+    primary: '#1e90ff',
+    secondary: '#D1E8FF'
+  },
+  yellow: {
+    primary: '#e3bc08',
+    secondary: '#FDF1BA'
+  }
+};
 
 @Component({
   selector: 'app-root',
@@ -10,5 +35,31 @@ import { CalendarEvent } from 'angular-calendar';
 export class AppComponent {
   view: string = 'month';
   viewDate: Date = new Date();
-  events = [];
+
+  events: CalendarEvent[] = [{
+    start: subDays(startOfDay(new Date()), 1),
+    end: addDays(new Date(), 1),
+    title: 'A 3 day event',
+    color: colors.red,
+  }, {
+    start: startOfDay(new Date()),
+    title: 'An event with no end date',
+    color: colors.yellow,
+  }, {
+    start: subDays(endOfMonth(new Date()), 3),
+    end: addDays(endOfMonth(new Date()), 3),
+    title: 'A long event that spans 2 months',
+    color: colors.blue
+  }, {
+    start: addHours(startOfDay(new Date()), 2),
+    end: new Date(),
+    title: 'A draggable and resizable event',
+    color: colors.yellow,
+    resizable: {
+      beforeStart: true,
+      afterEnd: true
+    },
+    draggable: true
+  }];
+
 }
